40efe15de67b2db6cba0f66a99129d1463bf271d
Iker-Romero <ikersoftdev@gmail.com>
2022-10-12 13:00:49 +0200

    footer logos href updated to the online library

--- app/components/insertFooter/insertFooter.js
+++ app/components/insertFooter/insertFooter.js
@@ -3,21 +3,19 @@ const contactInfo = [
     name: "Iker Romero Caramés",
     socialNetworks: {
       github: {
-        icon: "/github-light.png",
+        icon: "https://res.cloudinary.com/dvwva99mi/image/upload/v1665572313/logos/github-light_bblejx.png",
         link: "https://github.com/Iker-Romero",
       },
       linkedin: {
-        icon: "public/174857.png",
+        icon: "https://res.cloudinary.com/dvwva99mi/image/upload/v1665572313/logos/linkedin_tk9sdm.png",
         link: "https://www.linkedin.com/in/iker-romero-645b70251/",
       },
     },
   },
 ];
 
-export const insertFooter = async () => {
-  // try {
-  //   await insertPlayers(getPlayers("http://localhost:3000/players"));
-    const footer = `
+export const insertFooter = () => {
+  const footer = `
     <footer>
       <p class="copyright">© 2022 | Developed by: ${contactInfo[0].name}.</p>
       <ul class="contact-icons">
@@ -28,9 +26,6 @@ export const insertFooter = async () => {
         </li>
       </ul>
     </footer>
-    `
-    document.body.insertAdjacentHTML("beforeend", footer)
-  // } catch (error) {
-  //   console.log(error);
-  // }
+    `;
+  document.body.insertAdjacentHTML("beforeend", footer);
 };
